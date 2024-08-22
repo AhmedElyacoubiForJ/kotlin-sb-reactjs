7de218dae1e9e3c2f842f1f7d8135b77d0ce1178
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-23 00:21:06 +0200

    create author endpoint test

--- sb-kotlin-backend/2-2-testing-create-author-pt/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/AuthorController.kt
+++ sb-kotlin-backend/2-2-testing-create-author-pt/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/AuthorController.kt
@@ -4,6 +4,8 @@ import edu.yacoubi.bookstore.domain.dto.AuthorDto
 import edu.yacoubi.bookstore.service.IAuthorService
 import edu.yacoubi.bookstore.toAuthorDto
 import edu.yacoubi.bookstore.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 class AuthorController(private val authorService: IAuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
-        return authorService.save(
+    fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(
             authorDto.toAuthorEntity()
         ).toAuthorDto()
+
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 }
