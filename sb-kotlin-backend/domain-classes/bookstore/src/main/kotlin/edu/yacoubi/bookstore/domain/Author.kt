19d641a9a7d09608ba647c31e344f2ac06e1a146
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-22 17:11:42 +0200

    data classes

--- sb-kotlin-backend/domain-classes/bookstore/src/main/kotlin/edu/yacoubi/bookstore/domain/Author.kt
+++ sb-kotlin-backend/domain-classes/bookstore/src/main/kotlin/edu/yacoubi/bookstore/domain/Author.kt
@@ -1,3 +1,3 @@
 package edu.yacoubi.bookstore.domain
 
-class Author(var id: String, var name: String, var age: Int, var description: String, var image: String)
+class Author(var id: Long?, var name: String, var age: Int, var description: String, var image: String)
