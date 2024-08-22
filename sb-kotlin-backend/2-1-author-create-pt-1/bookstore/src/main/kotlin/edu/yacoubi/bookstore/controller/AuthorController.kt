dc3271bc9f1a7e583298878fcebe055307059f6f
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-22 21:36:20 +0200

    author endpoint

--- sb-kotlin-backend/2-1-author-create-pt-1/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/AuthorController.kt
+++ sb-kotlin-backend/2-1-author-create-pt-1/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/AuthorController.kt
@@ -1,15 +1,18 @@
 package edu.yacoubi.bookstore.controller
 
 import edu.yacoubi.bookstore.domain.dto.AuthorDto
+import edu.yacoubi.bookstore.service.IAuthorService
+import edu.yacoubi.bookstore.toAuthorDto
+import edu.yacoubi.bookstore.toAuthorEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AuthorController {
+class AuthorController(private val authorService: IAuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto) {
-        // Implement author creation logic here
+    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
+        return authorService.save(authorDto.toAuthorEntity()).toAuthorDto()
     }
 }
