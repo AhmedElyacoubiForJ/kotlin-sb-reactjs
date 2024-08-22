9534a7926f01cc198ebd7ec03543cee22efece32
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-22 22:31:11 +0200

    setup to test

---
+++ sb-kotlin-backend/2-2-testing-create-author-pt/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/AuthorController.kt
@@ -0,0 +1,20 @@
+package edu.yacoubi.bookstore.controller
+
+import edu.yacoubi.bookstore.domain.dto.AuthorDto
+import edu.yacoubi.bookstore.service.IAuthorService
+import edu.yacoubi.bookstore.toAuthorDto
+import edu.yacoubi.bookstore.toAuthorEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthorController(private val authorService: IAuthorService) {
+
+    @PostMapping(path = ["/v1/authors"])
+    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
+        return authorService.save(
+            authorDto.toAuthorEntity()
+        ).toAuthorDto()
+    }
+}
