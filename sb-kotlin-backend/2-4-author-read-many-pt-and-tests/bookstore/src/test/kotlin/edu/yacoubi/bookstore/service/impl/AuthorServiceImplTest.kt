1a60c0b121e0b48f3e946157a9ed713b5fa836a8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-25 13:42:32 +0200

    author service read all integration tests

--- sb-kotlin-backend/2-4-author-read-many-pt-and-tests/bookstore/src/test/kotlin/edu/yacoubi/bookstore/service/impl/AuthorServiceImplTest.kt
+++ sb-kotlin-backend/2-4-author-read-many-pt-and-tests/bookstore/src/test/kotlin/edu/yacoubi/bookstore/service/impl/AuthorServiceImplTest.kt
@@ -2,13 +2,18 @@ package edu.yacoubi.bookstore.service.impl
 
 import edu.yacoubi.bookstore.repository.AuthorRepository
 import edu.yacoubi.bookstore.testAuthorEntityA
+import edu.yacoubi.bookstore.testAuthorEntityB
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest
+@DirtiesContext(
+    classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD
+)
 class AuthorServiceImplTest @Autowired constructor(
     private val underTest: AuthorServiceImpl,
     private val authorRepository: AuthorRepository) {
@@ -20,8 +25,21 @@ class AuthorServiceImplTest @Autowired constructor(
 
         val retrievedAuthor = authorRepository.findByIdOrNull(savedAuthor.id!!)
         assertThat(retrievedAuthor).isNotNull()
-        assertThat(retrievedAuthor).isEqualTo(
-            testAuthorEntityA(savedAuthor.id)
-        )
+        assertThat(retrievedAuthor).isEqualTo(testAuthorEntityA(savedAuthor.id))
+    }
+
+    @Test
+    fun `test that list authors should returns empty list when no authors in the database`() {
+        val result = underTest.list();
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun `test that list authors should return all authors from the database`() {
+        authorRepository.saveAll(listOf(testAuthorEntityA(1), testAuthorEntityB(2)))
+
+        val result = underTest.list();
+        assertThat(result).hasSize(2)
+        assertThat(result).contains(testAuthorEntityA(1), testAuthorEntityB(2))
     }
 }
