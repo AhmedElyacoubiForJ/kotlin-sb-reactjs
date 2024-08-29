945b1b6a5e41c966d307e7ef62731f473f3c6a77
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-08-29 14:34:27 +0200

    book read many endpoint implementation

--- sb-kotlin-backend/3-2-book-read-many-pt-and-tests/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/BookController.kt
+++ sb-kotlin-backend/3-2-book-read-many-pt-and-tests/bookstore/src/main/kotlin/edu/yacoubi/bookstore/controller/BookController.kt
@@ -7,6 +7,7 @@ import edu.yacoubi.bookstore.toBookSummary
 import edu.yacoubi.bookstore.toBookSummaryDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -30,5 +31,10 @@ class BookController(val bookService: IBookService) {
         } catch (ex: IllegalStateException) {
             return ResponseEntity(HttpStatus.BAD_REQUEST)
         }
+    }
+
+    @GetMapping(path = ["/books/v1"])
+    fun getAllBooks(): List<BookSummaryDto> {
+        return bookService.getAllBooks().map { it.toBookSummaryDto() }
     }
 }
--- sb-kotlin-backend/3-2-book-read-many-pt-and-tests/bookstore/src/main/kotlin/edu/yacoubi/bookstore/service/impl/BookServiceImpl.kt
+++ sb-kotlin-backend/3-2-book-read-many-pt-and-tests/bookstore/src/main/kotlin/edu/yacoubi/bookstore/service/impl/BookServiceImpl.kt
@@ -29,4 +29,8 @@ class BookServiceImpl(
         val savedBook = bookRepository.save(normalisedBook.toBookEntity(author))
         return Pair(savedBook, !isExists)
     }
+
+    override fun getAllBooks(): List<BookEntity> {
+        return bookRepository.findAll()
+    }
 }
